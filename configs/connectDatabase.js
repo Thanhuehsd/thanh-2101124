02048a7dc16b33f59a0fed6c1536390cb38f3178
Thanhuehsd <tvthanh2101124@student.ctuet.edu.vn>
2024-11-07 10:22:25 +0700

    buoi5

--- configs/connectDatabase.js
+++ configs/connectDatabase.js
@@ -2,7 +2,7 @@ import mysql from 'mysql2'
 const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
-    database: 'users',
+    database: 'ltw',
     password: ''
 })
 const connection = pool.promise();
