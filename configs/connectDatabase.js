e0ff7aa3cd068d07847404008182dbd6b42287ba
Thanhuehsd <tvthanh2101124@student.ctuet.edu.vn>
2024-11-18 14:58:11 +0700

    buoi5

--- configs/connectDatabase.js
+++ configs/connectDatabase.js
@@ -2,7 +2,7 @@ import mysql from 'mysql2'
 const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
-    database: 'ltw',
+    database: 'buoi5',
     password: ''
 })
 const connection = pool.promise();
--- controller/ContactController.js
+++ controller/ContactController.js
@@ -2,8 +2,8 @@ import express from "express"
 const getContact = (req, res) => {
     return res.render("home", { data: { title: 'Page Contact',
         page:'contact', 
-        name: 'tương vĩnh thành', 
-        email: 'ht@gmail.com',
-        phonenumber: '123456' } })
+        name: 'Huỳnh Duy Đặng', 
+        email: 'hddang2100514@gmail.com',
+        phonenumber: '1234567890' } })
 }
 export default getContact
